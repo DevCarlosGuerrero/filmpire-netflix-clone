f2bce38d47ef208b8a1a008a83155e83c08e4254
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2022-06-14 15:38:39 -0400

    Update to React V18

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { App } from './components';
 // import { Actors, MovieInformation, Movies, Profile } from './pages';
-
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 const theme = createTheme([]);
 
-ReactDOM.render(
+root.render(
   <ThemeProvider theme={theme}>
     <Router>
       <App />
     </Router>
-  </ThemeProvider>,
-  document.getElementById('root')
+  </ThemeProvider>
 );
-
-// import React from 'react';
-// import createRoot from 'react-dom/client';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-// import { App } from './components';
-// // import { Actors, MovieInformation, Movies, Profile } from './pages';
-// const rootElement = document.getElementById('root');
-// const root = createRoot(rootElement);
-// const theme = createTheme([]);
-
-// root.render(
-//   <ThemeProvider theme={theme}>
-//     <Router>
-//       <App />
-//     </Router>
-//   </ThemeProvider>
-// );
