3175dc80bd60b72008bcff87598734a292774e37
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2022-06-14 07:32:30 -0400

    Implement Navbar

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { App } from './components';
 import { Actors, MovieInformation, Movies, Profile } from './pages';
 
+const theme = createTheme([]);
+
 ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/movie/:id" element={<MovieInformation />} />
-      <Route path="/actors/:id" element={<Actors />} />
-      <Route path="/profile/:id" element={<Profile />} />
-    </Routes>
-  </Router>,
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/movie/:id" element={<MovieInformation />} />
+        <Route path="/actors/:id" element={<Actors />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </Router>
+  </ThemeProvider>,
   document.getElementById('root')
 );
