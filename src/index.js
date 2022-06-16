81af4c4ca97a20d39c5372dc071296bcf6102732
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2022-06-16 09:02:51 -0400

    Initial Redux Implementation  ⚛️

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { Provider } from 'react-redux';
 import { App } from './components';
-// import { Actors, MovieInformation, Movies, Profile } from './pages';
+import store from './app/store';
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 const theme = createTheme([]);
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <Router>
-      <App />
-    </Router>
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
+  </Provider>
 );
