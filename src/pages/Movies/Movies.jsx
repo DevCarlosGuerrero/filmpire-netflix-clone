2d283e781f2e02cc5705606eab3f7fd523f6a6ee
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2022-06-18 10:41:01 -0400

    Implement movielist and movie component. 🎬

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -7,12 +7,37 @@ import {
 } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '../../components';
 
 function Movies() {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
 
-  return <div>Movies</div>;
+  if (!data.results.length) {
+    return (
+      <Box display="flex" justifyContent="center" mt="20px">
+        <Typography variant="h4">
+          No Movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occured.';
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 }
 
 export default Movies;
