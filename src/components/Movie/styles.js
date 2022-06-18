9ad51b844d1c6b68a63de4e000f104d5a69d5461
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2022-06-18 11:18:58 -0400

    Implement movie component styling & rating 🎬

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -4,6 +4,27 @@ export default makeStyles((theme) => ({
   movie: {
     padding: '10px',
   },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      textDecoration: 'none',
+    },
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    transition: 'all 0.2s ease-in-out',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
   title: {
     color: theme.palette.text.primary,
     textOverflow: 'ellipsis',
