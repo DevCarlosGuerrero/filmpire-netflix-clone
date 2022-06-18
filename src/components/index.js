2907464bea513252a2f86fb3d7e4ffcacafe8aeb
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2022-06-18 10:41:59 -0400

    Implement movie list and movies component. 🎬

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as App } from './App';
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as MovieList } from './MovieList/MovieList';
+export { default as Movie } from './Movie/Movie';
