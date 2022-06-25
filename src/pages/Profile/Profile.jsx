151dd2daa7cc6e09f073b459e678dec99680d5bb
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2022-06-25 10:26:51 -0400

    Finalize authentication implementation

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp, Favorite } from '@mui/icons-material';
+
 import { userSelector } from '../../features/auth';
-// Get access to profile name or id from redux state
-// Display in profile component
 
 function Profile() {
   const { user } = useSelector(userSelector);
-  console.log(user);
-  return <div>Welcome, {user.username}</div>;
+  const favoriteMovies = [];
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>Favorite Movies!</Box>
+      )}
+    </Box>
+  );
 }
 
 export default Profile;
