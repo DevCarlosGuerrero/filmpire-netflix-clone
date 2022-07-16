a2fcebd63614af4170bb44a8fde761d426744966
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2022-07-16 15:59:41 -0400

    Finalized initial Movie Info Page Implementation

--- src/pages/MovieInformation/styles.js
+++ src/pages/MovieInformation/styles.js
@@ -14,9 +14,9 @@ export default makeStyles((theme) => ({
     borderRadius: '20px',
     boxShadow: '0.5em 1em 1em rgba(64,64, 70, 0.2)',
     width: '80%',
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('md')]: {
       margin: '0 auto',
-      width: '50%%',
+      width: '50%',
       height: '350px',
     },
     [theme.breakpoints.down('sm')]: {
@@ -24,6 +24,24 @@ export default makeStyles((theme) => ({
       width: '100%',
       height: '350px',
       marginBottom: '30px',
+    },
+  },
+  genresContainers: {
+    margin: '10px 0 !important',
+    display: 'flex',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+  },
+  genreImage: {
+    filter: theme.palette.mode === 'dark' && 'invert(1)',
+    marginRight: '10px',
+  },
+  links: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    [theme.breakpoints.down('sm')]: {
+      padding: '0.5rem 1rem',
     },
   },
 }));
